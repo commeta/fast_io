67b53180d078be74be96589c4a31a4a5d1ed45f6
Евгений <40506054+commeta@users.noreply.github.com>
2024-03-30 00:17:49 +0300

    Update fast_io.c

--- fast_io.c
+++ fast_io.c
@@ -9,7 +9,9 @@
 
 /* Декларация функций */
 PHP_FUNCTION(find_value_by_key);
+PHP_FUNCTION(indexed_find_value_by_key);
 PHP_FUNCTION(write_key_value_pair);
+PHP_FUNCTION(indexed_write_key_value_pair);
 PHP_FUNCTION(delete_key_value_pair);
 
 /* Запись аргументов функций */
@@ -18,7 +20,18 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_find_value_by_key, 0, 2, IS_STRI
     ZEND_ARG_TYPE_INFO(0, index_key, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_indexed_find_value_by_key, 0, 2, IS_STRING, 1)
+    ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, index_key, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_write_key_value_pair, 0, 3, IS_VOID, 0)
+    ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, index_key, IS_STRING, 0)
+    ZEND_ARG_TYPE_INFO(0, index_val, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_indexed_write_key_value_pair, 0, 3, IS_VOID, 0)
     ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, index_key, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, index_val, IS_STRING, 0)
@@ -32,7 +45,9 @@ ZEND_END_ARG_INFO()
 /* Регистрация функций */
 const zend_function_entry fast_io_functions[] = {
     PHP_FE(find_value_by_key, arginfo_find_value_by_key)
+    PHP_FE(indexed_find_value_by_key, arginfo_indexed_find_value_by_key)
     PHP_FE(write_key_value_pair, arginfo_write_key_value_pair)
+    PHP_FE(indexed_write_key_value_pair, arginfo_indexed_write_key_value_pair)
     PHP_FE(delete_key_value_pair, arginfo_delete_key_value_pair)
     PHP_FE_END
 };
@@ -76,6 +91,26 @@ PHP_FUNCTION(find_value_by_key) {
     }
 }
 
+PHP_FUNCTION(indexed_find_value_by_key) {
+    char *filename;
+    size_t filename_len;
+    char *index_key;
+    size_t index_key_len;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "ss", &filename, &filename_len, &index_key, &index_key_len) == FAILURE) {
+        RETURN_FALSE;
+    }
+
+    char *result = indexed_find_value_by_key(filename, index_key);
+    
+    if (result != NULL) {
+        RETVAL_STRING(result);
+        free(result);
+    } else {
+        RETURN_NULL();
+    }
+}
+
 PHP_FUNCTION(write_key_value_pair) {
     char *filename;
     size_t filename_len;
@@ -84,11 +119,28 @@ PHP_FUNCTION(write_key_value_pair) {
     char *index_val;
     size_t index_val_len;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS(), "ss", &filename, &filename_len, &index_key, &index_key_len, &index_val, &index_val_len) == FAILURE) {
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "sss", &filename, &filename_len, &index_key, &index_key_len, &index_val, &index_val_len) == FAILURE) {
         RETURN_FALSE;
     }
 
     write_key_value_pair(filename, index_key, index_val);
+    
+    RETURN_NULL();
+}
+
+PHP_FUNCTION(indexed_write_key_value_pair) {
+    char *filename;
+    size_t filename_len;
+    char *index_key;
+    size_t index_key_len;
+    char *index_val;
+    size_t index_val_len;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "sss", &filename, &filename_len, &index_key, &index_key_len, &index_val, &index_val_len) == FAILURE) {
+        RETURN_FALSE;
+    }
+
+    indexed_write_key_value_pair(filename, index_key, index_val);
     
     RETURN_NULL();
 }
