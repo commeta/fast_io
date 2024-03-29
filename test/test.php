302d40b0a85d609facb2803825a3796b3f87cc33
Евгений <40506054+commeta@users.noreply.github.com>
2024-03-30 02:22:42 +0300

    Update test.php

--- test/test.php
+++ test/test.php
@@ -1,34 +1,33 @@
 <?php
+print_r(__DIR__ . '/fast_io2.dat' . "\n");
 
 for($i=0; $i <=10; $i++){
-	write_key_value_pair('fast_io2.dat', 'index' . $i, 'data' . $i);
+	write_key_value_pair(__DIR__ . '/fast_io2.dat', 'index_' . $i, 'data_write_key_value_pair_' . $i);
 
-	print_r([$i]);
+	print_r($i . ', ');
 }
 
 print_r([
-	find_value_by_key('fast_io2.dat', 'index5')
+	find_value_by_key(__DIR__ . '/fast_io2.dat', 'index_5')
 ]);
 
-delete_key_value_pair('fast_io2.dat', 'index5');
+delete_key_value_pair(__DIR__ . '/fast_io2.dat', 'index_5');
 
 print_r([
-	find_value_by_key('fast_io2.dat', 'index5')
+	find_value_by_key(__DIR__ . '/fast_io2.dat', 'index_5')
 ]);
 
 
-for($i=0; $i <=10; $i++){
-	indexed_write_key_value_pair('fast_io3.dat', 'index' . $i, 'data' . $i);
 
-	print_r([$i]);
+print_r(__DIR__ . '/fast_io3.dat' . "\n");
+
+for($i=0; $i <=10; $i++){
+	indexed_write_key_value_pair(__DIR__ . '/fast_io3.dat', 'index_' . $i, 'data_indexed_write_key_value_pair_' . $i);
+
+	print_r($i . ', ');
 }
 
 print_r([
-	indexed_find_value_by_key('fast_io3.dat', 'index5')
+	indexed_find_value_by_key(__DIR__ . '/fast_io3.dat', 'index_8')
 ]);
-
-
-
-
-
 
