324e0e9f4d90a4331e7846d620d9865958a79996
Евгений <40506054+commeta@users.noreply.github.com>
2024-03-31 03:34:06 +0300

    Update test.php

--- test/test.php
+++ test/test.php
@@ -1,6 +1,5 @@
 <?php
 print_r(__DIR__ . '/fast_io2.dat' . "\n");
-
 for($i=0; $i <=10; $i++){
 	write_key_value_pair(__DIR__ . '/fast_io2.dat', 'index_' . $i, 'data_write_key_value_pair_' . $i);
 
@@ -10,9 +9,7 @@ for($i=0; $i <=10; $i++){
 print_r([
 	find_value_by_key(__DIR__ . '/fast_io2.dat', 'index_5')
 ]);
-
 delete_key_value_pair(__DIR__ . '/fast_io2.dat', 'index_5');
-
 print_r([
 	find_value_by_key(__DIR__ . '/fast_io2.dat', 'index_5')
 ]);
@@ -20,7 +17,6 @@ print_r([
 
 
 print_r(__DIR__ . '/fast_io3.dat' . "\n");
-
 for($i=0; $i <=10; $i++){
 	indexed_write_key_value_pair(__DIR__ . '/fast_io3.dat', 'index_' . $i, 'data_indexed_write_key_value_pair_' . $i);
 
@@ -32,90 +28,72 @@ print_r([
 	indexed_find_value_by_key(__DIR__ . '/fast_io3.dat', 'index_8')
 ]);
 
-indexed_delete_key(__DIR__ . '/fast_io3.dat', 'index_8');
+delete_key_value_pair(__DIR__ . '/fast_io3.dat.index', 'index_8');
 
 print_r([
 	indexed_find_value_by_key(__DIR__ . '/fast_io3.dat', 'index_8')
 ]);
 
-
-
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	write_key_value_pair(__DIR__ . '/fast_io22.dat', 'index_' . $i, 'data_write_key_value_pair_' . $i);
 }
-
 $time= microtime(true) - $start;
 echo "write_key_value_pair: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	find_value_by_key(__DIR__ . '/fast_io22.dat', 'index_' . $i);
 }
-
 $time= microtime(true) - $start;
 echo "find_value_by_key: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	find_value_by_key(__DIR__ . '/fast_io22.dat', 'index_10');
 }
-
 $time= microtime(true) - $start;
 echo "find_value_by_key repeat: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	delete_key_value_pair(__DIR__ . '/fast_io22.dat', 'index_' . $i);
 }
-
 $time= microtime(true) - $start;
 echo "delete_key_value_pair: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	indexed_write_key_value_pair(__DIR__ . '/fast_io33.dat', 'index_' . $i, 'data_write_key_value_pair_' . $i);
 }
-
 $time= microtime(true) - $start;
 echo "indexed_write_key_value_pair: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	indexed_find_value_by_key(__DIR__ . '/fast_io33.dat', 'index_' . $i);
 }
-
 $time= microtime(true) - $start;
 echo "indexed_find_value_by_key: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	indexed_find_value_by_key(__DIR__ . '/fast_io33.dat', 'index_10');
 }
-
 $time= microtime(true) - $start;
 echo "indexed_find_value_by_key repeat: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
 
 $start= microtime(true);
-
 for($i=0; $i <=10000; $i++){
 	indexed_delete_key(__DIR__ . '/fast_io33.dat', 'index_' . $i);
 }
-
 $time= microtime(true) - $start;
 echo "indexed_delete_key: ", $time, " (", sprintf('%.8f', ($time / 10000)), ")",  "\n";
 
