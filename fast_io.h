1f53bc2e0c44c2a9e7ab236cd1e7aa74fceddc7e
Евгений <40506054+commeta@users.noreply.github.com>
2024-04-30 12:29:21 +0300

    Update fast_io.h

--- fast_io.h
+++ fast_io.h
@@ -55,50 +55,34 @@ int lock_file(int fd, int lock_type) {
 
 // Структура для хранения динамического массива ключей и их значений
 typedef struct {
-    int (*values)[2];
+    int (*values)[3];
+    char **lines;
     size_t count;
-} KeyValueArray;
+} KeyValueLineArray;
 
-bool add_key_value(KeyValueArray *array, int value[2]) {
+bool add_key_value_line(KeyValueLineArray *array, int value[3], char *line) {
+    if (array == NULL) return false;
+
     array->count++;
-    array->values = erealloc(array->values, array->count * sizeof(int[2]));
+    array->values = erealloc(array->values, array->count * sizeof(int[3]));
     if(array->values == NULL) return false;
+
+    array->lines = erealloc(array->lines, array->count * sizeof(char *));
+    if(array->lines == NULL) return false;
     
+    array->lines[array->count - 1] = estrdup(line);
     array->values[array->count - 1][0] = value[0];
     array->values[array->count - 1][1] = value[1];
+    array->values[array->count - 1][2] = value[2];
+
     return true;
 }
 
-void free_key_value_array(KeyValueArray *array) {
+void free_key_value_line_array(KeyValueLineArray *array) {
     for (size_t i = 0; i < array->count; i++) {
+        efree(array->lines[i]);
         efree(array->values[i]);
     }    
     efree(array->values);
-}
-
-
-// Структура для хранения динамического массива ключей
-typedef struct {
-    char **keys;
-    size_t count;
-} KeyArray;
-
-
-// Функция для добавления ключа в массив ключей
-bool add_key(KeyArray *array, char *key) {
-    array->count++;
-    array->keys = erealloc(array->keys, array->count * (sizeof(size_t) + sizeof(char *)));
-    if(array->keys == NULL) return false;
-
-    array->keys[array->count - 1] = estrdup(key);
-    return true;
-}
-
-// Функция для освобождения памяти, выделенной под массив ключей
-void free_key_array(KeyArray *array) {
-    for (size_t i = 0; i < array->count; i++) {
-        efree(array->keys[i]);
-    }
-    efree(array->keys);
 }
 
