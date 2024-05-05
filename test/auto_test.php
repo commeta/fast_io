006a7b801d7f71093dd619c36e7baa027167c9d3
Евгений <40506054+commeta@users.noreply.github.com>
2024-05-05 10:42:39 +0300

    Create auto_test.php

---
+++ test/auto_test.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ * Fast_IO (pre-release beta) Extension for PHP 8
+ * https://github.com/commeta/fast_io
+ * 
+ * Copyright 2024 commeta <dcs-spb@ya.ru>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+$db_file = __DIR__ . '/fast_io.dat';
+
+
+// Check file_insert_line
+$file_insert_line_passed = true;
+
+for($ii = 0; $ii < 100; $ii++){
+    if(file_exists($db_file)) unlink($db_file);
+    $align = mt_rand(30, 16384);
+
+    $last_offset = 0;
+    
+    for($i=0; $i <= 500; $i++){
+        $shuffle = mt_rand(1, $align);
+
+        $str = 'index_' . $i . ' file_insert_line_' . $i . ' ' . str_pad('', $shuffle, '1234567890');
+        $file_offset = file_insert_line($db_file, $str, 2, $align);
+    
+        $file_line = file_get_keys($db_file, 0, 1, $last_offset, 1);
+
+        if(
+            empty($file_line[0]) ||
+            $file_line[0]['line_length'] != $align ||
+            mb_strlen($file_line[0]['line']) + 1 != $align
+        ){
+            $file_insert_line_passed = false;
+            break;
+        }
+
+        $file_str = $file_line[0]['line'];
+        $file_array = explode(' ', $file_str);
+        $str_array = explode(' ', $str);
+
+        if(
+            empty($file_array[0]) || 
+            $file_array[0] != 'index_' . $i ||
+            $file_array[1] != 'file_insert_line_' . $i ||
+            mb_strlen(trim($file_str)) != mb_strlen(mb_substr(trim($str), 0, $align - 1))
+            
+        ) {
+            $file_insert_line_passed = false;           
+            break;
+        }
+
+        if($file_offset == $last_offset) $last_offset += $align;  
+    }
+    
+    $analize = file_analize($db_file);
+     
+    if(
+        $last_offset - $align == $file_offset &&
+        $analize['total_characters'] == $last_offset &&
+        $analize['last_symbol'] == 10 &&
+        $analize['file_size'] == $last_offset &&
+        $analize['flow_interruption'] == 0 &&
+        $analize['min_length'] == $analize['max_length'] &&
+        $analize['min_length'] == $analize['avg_length'] &&
+        $analize['line_count'] == 501
+    ) {
+        $file_insert_line_passed = true;
+    } else {
+        $file_insert_line_passed = false;
+        break;
+    }
+}
+
+
+if($file_insert_line_passed) echo "Check file_insert_line alignment data - PASS\n";
+else echo "Check file_insert_line alignment data - ERROR\n";
+
